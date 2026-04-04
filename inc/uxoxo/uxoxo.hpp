bcca30e9b84c54936e6460b1942685461ea9bb02
snealbli <dev@nealblim.com>
2026-04-03 21:20:42 -0400

    Added 'uxoxo.hpp': uxoxo framework base module.

---
+++ inc/uxoxo/uxoxo.hpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+* uxoxo [core]                                                       uxoxo.hpp
+*
+*
+*
+* path:      /inc/uxoxo/uxoxo.hpp
+* link(s):   TBA
+* author(s): Samuel 'teer' Neal-Blim                          date: 2025.05.19
+******************************************************************************/
+
+#ifndef UXOXO_
+#define UXOXO_ 1
+
+
+#endif  // UXOXO_
