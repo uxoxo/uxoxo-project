fc60f68afaf05a64469ee87cbde9a791037fffa3
snealbli <dev@nealblim.com>
2026-04-14 20:59:23 -0400

    Updated 'uxoxo.hpp': added additional keywords/namespaces.

--- inc/uxoxo/uxoxo.hpp
+++ inc/uxoxo/uxoxo.hpp
@@ -13,6 +13,7 @@
 
 #include <djinterp/core/djinterp.hpp>
 
+
 ///////////////////////////////////////////////////////////////////////////////
 ///                  I.   KEYWORDS & NAMESPACE MACROS                       ///
 ///////////////////////////////////////////////////////////////////////////////
@@ -30,6 +31,17 @@
 // Used to specify that a unit of code deals individual UI components.
 #define U_KEYWORD_COMPONENT             component
 
+// U_KEYWORD_PLATFORM
+//   keyword: resolves to `platform`.
+// Specifies vendor-specific functionality.
+#define U_KEYWORD_PLATFORM              platform
+
+// U_KEYWORD_UI_TREE
+//   keyword: resolves to `tree`.
+// Used to specify that a unit of code deals with the central UI tree that is
+// central to the uxoxo framework.
+#define U_KEYWORD_UI_TREE               ui_tree
+
 // D_KEYWORD_TEMPLATES
 //   keyword: resolves to `templates`.
 // Used to specify that a unit of code deals with templates.
@@ -39,6 +51,12 @@
 //   keyword: resolves to `ui`.
 // Used to specify that a unit of code deals with templates.
 #define U_KEYWORD_UI                    ui
+
+// U_KEYWORD_WYSIWYG
+//   keyword: resolves to `wysiwyg`.
+// Used to specify that a unit of code deals the WYSIWYG editor (what you see
+// is what you get).
+#define U_KEYWORD_WYSIWYG               wysiwyg
 
 // ii.  namespace macros
 //////////////////////////////////////////
@@ -52,6 +70,10 @@
 // component templates.
 #define NS_COMPONENT                    D_NAMESPACE(U_KEYWORD_COMPONENT)
 
+// NS_PLATFORM
+//   namespace: vendor-specific UI elements, layouts, widgets, and more.
+#define NS_PLATFORM                     D_NAMESPACE(U_KEYWORD_PLATFORM)
+
 // NS_TEMPLATES
 //   namespace: the `templates` namespace containing templates for various 
 // individual UI components, UI layouts, and more.
@@ -61,7 +83,22 @@
 //   namespace: vendor-specific UI elements, layouts, widgets, and more.
 #define NS_UI                           D_NAMESPACE(U_KEYWORD_UI)
 
+// NS_UI_TREE
+//   namespace: the `tree` namespace, corresponding to the central UI element
+// tree.
+#define NS_UI_TREE                      D_NAMESPACE(U_KEYWORD_UI_TREE)
 
+// NS_WYSIWYG
+//   namespace:  WYSIWYG editor namespace (what you see is what you get).
+#define NS_WYSIWYG                      D_NAMESPACE(U_KEYWORD_WYSIWYG)
+
+// NS_GLFW
+//   namespace: vendor-specific UI elements, layouts, widgets, and more.
+#define NS_GLFW                         D_NAMESPACE(glfw)
+
+// NS_IMGUI
+//   namespace: vendor-specific UI elements, layouts, widgets, and more.
+#define NS_IMGUI                        D_NAMESPACE(imgui)
 
 
 #endif  // UXOXO_
